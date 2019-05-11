20921f39d0648fbf8e46a8ce5550b815391056a7
Henrik Himmelreich <hepahi@web.de>
2019-05-11 22:24:02 +0200

    Update User.java

--- src/main/java/hackathon/healthyearth/User.java
+++ src/main/java/hackathon/healthyearth/User.java
@@ -6,14 +6,24 @@ public class User {
     private int points;
     // TODO add personal data
 
+    private List<Challenge> currentChallenges;
+
     public User(String username, String password) {
         this.username = username;
         this.password = password;
         this.points = 0;
+
+        //create and assign random challenges
+        this.currentChallenges = getNewChallenges();
+
     }
 
     public boolean passwordMatches(String password) {
         return this.password.equals(password);
+    }
+
+    public void setCurrentChallenges(List<Challenge> newChallenges) {
+      currentChallenges = newChallenges;
     }
 
 }
